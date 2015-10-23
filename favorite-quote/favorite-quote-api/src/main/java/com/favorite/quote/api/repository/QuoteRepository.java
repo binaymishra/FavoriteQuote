6db604596c6ebfe4b732355ed40fbec6f48079e8
Binay Mishra <binaymishramca@gmail.com>
2015-10-23 09:39:46 +0530

    Repository.

---
+++ favorite-quote/favorite-quote-api/src/main/java/com/favorite/quote/api/repository/QuoteRepository.java
@@ -0,0 +1,5 @@
+package com.favorite.quote.api.repository;
+
+public interface QuoteRepository {
+
+}
---
+++ favorite-quote/favorite-quote-api/src/main/java/com/favorite/quote/api/repository/QuoteRepositoryImpl.java
@@ -0,0 +1,9 @@
+package com.favorite.quote.api.repository;
+
+import org.springframework.stereotype.Repository;
+
+
+@Repository("quoteRepository")
+public class QuoteRepositoryImpl implements QuoteRepository {
+
+}
