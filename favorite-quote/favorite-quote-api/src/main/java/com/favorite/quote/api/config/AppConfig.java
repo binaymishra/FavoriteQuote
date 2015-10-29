5cc2c320250848a78f0dcc63924f54135e1eda8b
Binay Mishra <binaymishramca@gmail.com>
2015-10-29 07:39:03 +0530

    Id generator Bean is configured.

--- favorite-quote/favorite-quote-api/src/main/java/com/favorite/quote/api/config/AppConfig.java
+++ favorite-quote/favorite-quote-api/src/main/java/com/favorite/quote/api/config/AppConfig.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import com.favorite.quote.api.business.CounterIdGenerator;
+import com.favorite.quote.api.business.IdGenerator;
+
 
 
 
@@ -16,6 +21,7 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 @ComponentScan("com.favorite.quote.api")
 public class AppConfig {
 	
+
 	@Autowired
 	private DataSource dataSource; 
 	
@@ -32,5 +38,18 @@ public class AppConfig {
 		DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
 		transactionManager.setDataSource(dataSource);
 		return transactionManager;
+	}
+	
+	@Bean(name = { "aothorIdGenerator" })
+	public IdGenerator getAuthorIdGenerator(DataSource dataSource) {
+		JdbcTemplate template = new JdbcTemplate(dataSource);
+		CounterIdGenerator generator = new CounterIdGenerator(template.queryForObject(IdGenerator.MAX_AUTHOR_ID, Long.class));
+		return generator;
+	}
+	@Bean(name = { "quoteIdGenerator" })
+	public IdGenerator getQuoteIdGenerator(DataSource dataSource) {
+		JdbcTemplate template = new JdbcTemplate(dataSource);
+		CounterIdGenerator generator = new CounterIdGenerator(template.queryForObject(IdGenerator.MAX_QUOTE_ID, Long.class));
+		return generator;
 	}
 }
