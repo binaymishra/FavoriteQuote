b596c5559af1d6299882672657385315abb08298
Binay Mishra <binaymishramca@gmail.com>
2015-11-21 09:03:07 +0530

    point cut corrected

--- favorite-quote/favorite-quote-api/src/main/java/com/favorite/quote/api/business/aspects/QuoteFileServiceLoggingAspect.java
+++ favorite-quote/favorite-quote-api/src/main/java/com/favorite/quote/api/business/aspects/QuoteFileServiceLoggingAspect.java
@@ -9,6 +9,12 @@ import org.springframework.stereotype.Component;
 import com.google.common.base.Stopwatch;
 
 
+/**
+ * Logging Aspect class for QuoteFileService class.
+ * 
+ * @author Binay Mishra
+ *
+ */
 @Aspect
 @Component
 public class QuoteFileServiceLoggingAspect {
@@ -17,14 +23,54 @@ public class QuoteFileServiceLoggingAspect {
 	
 	private Stopwatch stopwatch = Stopwatch.createUnstarted(); 
 	
+	/**
+	 * This method aspect is invoked before method  QuoteFileService.findAllQuotes() call.
+	 */
 	@Before("execution(* com.favorite.quote.api.business.QuoteFileServiceImpl.findAllQuotes())")
 	public void beforefindAllQuotes(){
 		LOGGER.info("Method: findAllQuotes() start.");
 		stopwatch.start();
 	}
 
-	@AfterReturning("execution(* com.favorite.quote.api.business.QuoteFileServiceImpl.findAllQuotes())")
+	/**
+	 * This method aspect is invoked after return of method QuoteFileService.findAllQuotes() call.
+	 */
+	@AfterReturning("execution(* com.favorite.quote.api.business.QuoteFileServiceImpl.findAllQuotes(..))")
 	public void afterReturningfindAllQuotes(){
 		LOGGER.info("Method: findAllQuotes() returned. Time elapsed "+stopwatch.stop()+".");
+	}
+	
+	/**
+	 * This method aspect is invoked before method  QuoteFileService.findQuoteById() call.
+	 */
+	@Before("execution(* com.favorite.quote.api.business.QuoteFileServiceImpl.findQuoteById(..))")
+	public void beforefindQuoteById(){
+		LOGGER.info("Method: findQuoteById() start.");
+		stopwatch.start();
+	}
+	
+	/**
+	 * This method aspect is invoked after return of method QuoteFileService.findQuoteById() call.
+	 */
+	@AfterReturning("execution(* com.favorite.quote.api.business.QuoteFileServiceImpl.findQuoteById(..))")
+	public void afterReturningfindQuoteById(){
+		LOGGER.info("Method: findQuoteById() returned. Time elapsed "+stopwatch.stop()+".");
+	}
+	
+	/**
+	 * This method aspect is invoked before method  QuoteFileService.findQuotesByAuthor() call.
+	 */
+	@Before("execution(* com.favorite.quote.api.business.QuoteFileServiceImpl.findQuotesByAuthor(..))")
+	public void beforefindQuotesByAuthor(){
+		LOGGER.info("Method: findQuotesByAuthor() start.");
+		stopwatch.start();
+	}
+	
+	/**
+	 * This method aspect is invoked after return of method QuoteFileService.findQuotesByAuthor() call.
+	 */
+	@AfterReturning("execution(* com.favorite.quote.api.business.QuoteFileServiceImpl.findQuotesByAuthor())")
+	public void afterReturningQuotesByAuthor(){
+		LOGGER.info("Method: findQuotesByAuthor() returned. Time elapsed "+stopwatch.stop()+".");
 	}
 }
--- favorite-quote/favorite-quote-api/src/main/java/com/favorite/quote/api/business/exception/QuoteNotFoundException.java
+++ favorite-quote/favorite-quote-api/src/main/java/com/favorite/quote/api/business/exception/QuoteNotFoundException.java
@@ -1,9 +1,13 @@
 package com.favorite.quote.api.business.exception;
 
+/**
+ * @author Binay Mishra
+ *
+ */
 public class QuoteNotFoundException extends RuntimeException {
 
 	/**
-	 * 
+	 * Custom exception class.
 	 */
 	private static final long serialVersionUID = 1L;
 	
