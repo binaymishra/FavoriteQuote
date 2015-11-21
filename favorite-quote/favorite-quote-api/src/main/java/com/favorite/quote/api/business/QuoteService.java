88695cc550c2e9f8210172834c47de00823e9270
Binay Mishra <binaymishramca@gmail.com>
2015-11-21 08:50:48 +0530

    Java Doc added.

--- favorite-quote/favorite-quote-api/src/main/java/com/favorite/quote/api/business/QuoteService.java
+++ favorite-quote/favorite-quote-api/src/main/java/com/favorite/quote/api/business/QuoteService.java
@@ -6,9 +6,35 @@ import java.util.Collection;
 import com.favorite.quote.api.domain.Author;
 import com.favorite.quote.api.domain.Quote;
 
+/**
+ * This is standard interface, through which all the services are exposed.
+ * 
+ * @author Binay Mishra
+ *
+ */
 public interface QuoteService {
+	
+	/**
+	 * This method returns all the Quotes from the repository.
+	 * 
+	 * @return
+	 */
 	public Collection<Quote> findAllQuotes();
+	
+	/**
+	 * This method returns unique quote using the unique quote id.
+	 * 
+	 * @param id
+	 * @return
+	 */
 	public Quote findQuoteById(Long id);
+	
+	/**
+	 * This method returns collection of quote according to the given author.
+	 * 
+	 * @param author
+	 * @return
+	 */
 	public Collection<Quote> findQuotesByAuthor(Author author);
 	
 }
